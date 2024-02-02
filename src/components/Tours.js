7d27da75032acb82e5bd924af3c662dde8b52e2a
kumarrupesh2000 <kumarrupesh2000@gmail.com>
2024-02-02 15:51:59 +0530

    Update Tours.js

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -7,7 +7,7 @@ function Tours({tours, removeTour}) {
             </div>
             <div className='cards'>
                 {
-                    tours.map( (tour) => {
+                    tours.map((tour) => {
                         return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>
                     } )
                 }
